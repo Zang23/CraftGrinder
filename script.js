190e942234a47d18a2fbdfe2839e6ab545a10cb5
Alanis <149546408+AlanisCoutinho@users.noreply.github.com>
2024-04-11 23:26:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const header = document.querySelector(".cabecalhototal");
+const toggleClass = "sticky";
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.scrollY;
+    if (currentScroll > 150){
+        header.classList.add(toggleClass);
+    }
+    else{
+        header.classList.remove(toggleClass);
+    }
+})
+
+
+/*
+function explodir(event){
+var expl = document.createElement("img");
+expl.setAttribute("src", "../img/explosion.gif");
+document.getElementsByClassName("cont_menor") [1].appendChild(expl);
+event.preventDefault();
+}
+*/
