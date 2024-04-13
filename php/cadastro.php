cd0e9df96bb6164ee142ab8ac8842254e55f93f7
Zang_23 <thiagoaraujo20005@gmail.com>
2024-04-13 03:54:53 -0300

    create: cadastro.php ,conexao.php

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,3 +1,18 @@
 <?php
 
+require 'conexao.php';
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$user = $_POST['user'];
+
+
+if(isset($email)){
+    $sql_code = $pdo->prepare("INSERT INTO tbclientes VALUES (null,?,?,?)");
+    $sql_code->execute([$_POST['email'], $_POST['senha'], $_POST['user']]);
+
+
+    echo "feito com sucesso";
+}
+
 ?>
---
+++ php/conexao.php
@@ -0,0 +1,7 @@
+<?php
+    
+    $pdo = new PDO('mysql:host=localhost;dbname=dbcraftgrinder' , 'root', '');
+    $pdo-> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+
+?>      
