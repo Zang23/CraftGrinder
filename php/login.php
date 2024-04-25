a294f582516931d26ae020e6e03a6fa2d0ac06cf
Zang_23 <125692776+Zang23@users.noreply.github.com>
2024-04-24 22:21:33 -0300

    Alterado: login.php

--- php/login.php
+++ php/login.php
@@ -2,11 +2,25 @@
 
 require 'conexao.php';
 
+$sql_code = "SELECT email, senha FROM tbclientes";
+$prepare = $pdo->prepare($sql_code);
+$count = $prepare->execute();
+
+$usuarios = $prepare->fetchAll();
+
 $nome_email = $_POST['nome_email'];
 $senha = $_POST['senha'];
 
 if(strstr($nome_email, "@gmail.com")){
     $email = $nome_email;
+    
+    foreach($usuarios as $usuario){
+        if($email == $usuario['email'] and $senha == $usuario['senha']){
+            header('location: ../html/index.html');
+        }else{
+            echo "Ocorreu algum erro";
+        }
+    }
 }else{
     $nome = $nome_email;
 }
