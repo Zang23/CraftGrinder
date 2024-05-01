5dd669b80ccecc7066db574a5c4bf81065278156
Zang_23 <thiagoaraujo20005@gmail.com>
2024-05-01 16:58:06 -0300

    refatorado: scriptCadastro

--- php/scriptCadastro.php
+++ php/scriptCadastro.php
@@ -5,33 +5,36 @@ require 'conexao.php';
 
 $email = $_POST['email'];
 $senha = $_POST['senha'];
+$senhaConfirmada = $_POST['senha_confirmada'];
 $user = $_POST['user'];
 
 $code_sql = "SELECT nome_usuario FROM tbclientes";
 $prepare = $pdo->prepare($code_sql);
 $count = $prepare->execute();
-
 $usuarios = $prepare->fetchAll();
 
+$usuarioCorreto = true;
 
-
-if($_POST['senha'] != $_POST['senha_confirmada']){
-    header('location: ../html/cadastro.php?senhaErro');
-
-}else if(isset($user)){
-    foreach($usuarios as $usuario){
-        if($usuario['nome_usuario'] == $user){
-            header('location: ../html/cadastro.php?userErro');
-        }
+foreach($usuarios as $usuario){
+    if( $user == $usuario['nome_usuario']){
+        $usuarioCorreto = false;
+        header('location: ../html/cadastro.php?userErro');
+        break;
     }
-    
-}else if ($_POST['senha'] == $_POST['senha_confirmada'] && (isset($email))){
-    $sql_code = $pdo->prepare("INSERT INTO tbclientes VALUES (null,?,?,?)");
-    $sql_code->execute([$_POST['email'], $_POST['senha'], $_POST['user']]);
-
-    header('location: ../html/index.html');
 }
 
+if($_POST['senha'] != $_POST['senha_confirmada']){
+    $senhaCorreta = false;
+    header('location: ../html/cadastro.php?senhaErro');
+}else{
+    $senhaCorreta = true;
+}
+
+if($senhaCorreta == true && $usuarioCorreto == true){
+    $sql_code = $pdo->prepare("INSERT INTO tbclientes VALUES (null,?,?,?)");
+    $sql_code->execute([$_POST['email'], $_POST['senha'], $_POST['user']]);
+    header('location: ../html/index.html');
+}
 
 
 
