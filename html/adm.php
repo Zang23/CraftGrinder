170173445214d8a2aa62792662709b69505cda09
Zang_23 <125692776+Zang23@users.noreply.github.com>
2024-05-02 21:55:23 -0300

    refactor: scriptCadastro.php, login.php; refactor: login.html, index.html; substituido: adm.html --> adm.php

---
+++ html/adm.php
@@ -0,0 +1,86 @@
+
+<?php
+
+require_once 'conexao.php';
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/adm.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="index_fundo">
+
+    <header class="cabecalho_container">
+
+        <div class="cabecalho_superior_container">
+            <a href="index.html"><p class="cabecalho_titulo">CraftGrinder</p></a>
+        </div>
+        
+    </header>
+
+<div class="adm_container">
+
+    <div class="barra_lateral">
+        <div class="content_lateral">
+
+            <p class="sidebar_titulo">Administração</p>
+
+            <div class="lateral_options">
+
+                <a href=""><p>Inserir nova Farm</p></a>
+                <a href=""><p>Inserir nova Máquina</p></a>
+                <a href=""><p>Inserir nova Item</p></a>
+                <a href=""><p>Inserir novo Guia</p></a>
+                <a href=""><p>Inserir nova Atualização  </p></a>
+                <a href=""><p>Editar cursos</p></a>
+                <a href=""><p>Sair</p></a>
+                <a href=""><p>Inserir novo Artigo</p></a>
+
+            </div>
+        </div>
+    </div>
+
+    
+    <div class="container_central">
+
+        <div class="alinhamento_titulo_central">
+            <p class="titulo_central">Página do Diretor</p>
+        </div>
+        
+        <div class="diretor_pag">
+            
+            <p>Conferir e editar</p>
+                <div class="tabela">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Tipo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>exemplo</td>
+                            <td>exemplo</td>
+                        </tr>
+                    </tbody>
+                </table>
+                </div>
+        </div>
+
+    </div>
+
+</div>
+
+</div>
+</body>
+</html>
