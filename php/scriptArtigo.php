a636d0689eddaed369e84ac5cff947ca1cf1bbee
Zang_23 <thiagoaraujo20005@gmail.com>
2024-05-07 06:37:11 -0300

    criado: selectFormulario.js em javascript/selectFormulario.js criado: cadastroArtigo.html em html/adm/cadastro.html refatorado: scriptArtigo.php, adm.php deletado: farmCadastro.html realocado: html/adm.php --> html/adm/adm.php

--- php/scriptArtigo.php
+++ php/scriptArtigo.php
@@ -2,23 +2,99 @@
 
 require_once 'conexao.php';
 
+$tipoArtigo = $_POST['tipoArtigo'];
 
-if(isset($_POST['nomeFarm'])){
 
-    $nomeFarm = $_POST['nomeFarm'];
-    $minidescFarm = $_POST['miniDescFarm'];
-    $descFarm = $_POST['descFarm'];
-    $imagemFarm = $_POST['imagemFarm'];
-    $tipo = "Farm";
+
+if($tipoArtigo == "Farm"){
+
+    $nome = $_POST['nomeFarm'];
+    $minidesc = $_POST['miniDescFarm'];
+    $desc = $_POST['descFarm'];
+    $imagem = $_POST['imagemFarm'];
 
     $sql_code = $pdo->prepare("INSERT INTO tbfarm VALUES (null,?,?,?,?,?)");
-    $sql_code->execute([$nomeFarm, $descFarm, $minidescFarm, $imagemFarm,$tipo]);
+    $sql_code->execute([$nome, $desc, $minidesc, $imagem,$tipoArtigo]);
 
     $code_sql = $pdo->prepare("INSERT INTO tbartigo VALUES (null,?,?)");
-    $code_sql->execute([$nomeFarm, $tipo]);?>
+    $code_sql->execute([$nome, $tipoArtigo]);?>
 
-    <a href="../html/farmCadastro.html"><button>Adicionar mais uma farm</button></a>
-    <a href="../html/adm.php"><button>Voltar a tela de Admin</button></a><?php
+    <a href="../html/adm/cadastroArtigo.html"><button>Adicionar mais um artigo</button></a>
+    <a href="../html/adm/adm.php"><button>Voltar a tela de Admin</button></a><?php
 }
+
+if($tipoArtigo == "Guia"){
+
+    $nome = $_POST['nomeGuia'];
+    $minidesc = $_POST['miniDescGuia'];
+    $desc = $_POST['descGuia'];
+    $imagem = $_POST['imagemGuia'];
+
+    $sql_code = $pdo->prepare("INSERT INTO tbguia VALUES (null,?,?,?,?,?)");
+    $sql_code->execute([$nome, $desc, $minidesc, $imagem,$tipoArtigo]);
+
+    $code_sql = $pdo->prepare("INSERT INTO tbartigo VALUES (null,?,?)");
+    $code_sql->execute([$nome, $tipoArtigo]);?>
+
+    <a href="../html/adm/cadastroArtigo.html"><button>Adicionar mais um artigo</button></a>
+    <a href="../html/adm/adm.php"><button>Voltar a tela de Admin</button></a><?php
+}
+
+if($tipoArtigo == "Maquina"){
+
+    $nome = $_POST['nomeMaquina'];
+    $minidesc = $_POST['miniDescMaquina'];
+    $desc = $_POST['descMaquina'];
+    $imagem = $_POST['imagemMaquina'];
+
+    $sql_code = $pdo->prepare("INSERT INTO tbmaquina VALUES (null,?,?,?,?,?)");
+    $sql_code->execute([$nome, $desc, $minidesc, $imagem,$tipoArtigo]);
+
+    $code_sql = $pdo->prepare("INSERT INTO tbartigo VALUES (null,?,?)");
+    $code_sql->execute([$nome, $tipoArtigo]);?>
+
+    <a href="../html/adm/cadastroArtigo.html"><button>Adicionar mais um artigo</button></a>
+    <a href="../html/adm/adm.php"><button>Voltar a tela de Admin</button></a><?php
+}
+
+if($tipoArtigo == "Item"){
+
+    $nome = $_POST['nomeItem'];
+    $minidesc = $_POST['miniDescItem'];
+    $desc = $_POST['descItem'];
+    $imagem = $_POST['imagemItem'];
+
+    $sql_code = $pdo->prepare("INSERT INTO tbitem VALUES (null,?,?,?,?,?)");
+    $sql_code->execute([$nome, $desc, $minidesc, $imagem,$tipoArtigo]);
+
+    $code_sql = $pdo->prepare("INSERT INTO tbartigo VALUES (null,?,?)");
+    $code_sql->execute([$nome, $tipoArtigo]);?>
+
+    <a href="../html/adm/cadastroArtigo.html"><button>Adicionar mais um artigo</button></a>
+    <a href="../html/adm/adm.php"><button>Voltar a tela de Admin</button></a><?php
+}
+
+if($tipoArtigo == "Atualizacao"){
+
+    $nome = $_POST['nomeAtualizacao'];
+    $desc = $_POST['descAtualizacao'];
+    $imagem = $_POST['imagemAtualizacao'];
+
+    $sql_code = $pdo->prepare("INSERT INTO tbatualizacao VALUES (null,?,?,?,?)");
+    $sql_code->execute([$nome, $desc, $imagem,$tipoArtigo]);
+
+    $code_sql = $pdo->prepare("INSERT INTO tbartigo VALUES (null,?,?)");
+    $code_sql->execute([$nome, $tipoArtigo]);?>
+
+    <a href="../html/adm/cadastroArtigo.html"><button>Adicionar mais um artigo</button></a>
+    <a href="../html/adm/adm.php"><button>Voltar a tela de Admin</button></a><?php
+}
+
+
+
+
+
+
+
 
 ?>
