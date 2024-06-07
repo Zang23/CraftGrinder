f6a7622914a6e55d189e71b3a4961e5266565268
Segetto <161674677+Segetto@users.noreply.github.com>
2024-06-07 13:52:29 -0300

    adição da fonte monocraft.ttf e formatação do arquivo teste.php

--- javascript/API/teste.php
+++ javascript/API/teste.php
@@ -1,86 +1,206 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../../css/inventario.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&family=Tiny5&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <table class="tabela">
-        <tr><td id=""><p>a</p></td><td id=""><p>b</p></td><td id=""><p>a</p></td><td id=""><p>b</p></td><td id=""><p>a</p></td><td id=""><p>b</p></td><td id=""><p>a</p></td><td id=""><p>b</p></td><td id=""><p>b</p></td></tr>
-        <tr><td id=""><p>a</p></td><td id=""><p>b</p></td><td id=""><p>a</p></td><td id=""><p>b</p></td><td id=""><p>a</p></td><td id=""><p>b</p></td><td id=""><p>a</p></td><td id=""><p>b</p></td><td id=""><p>b</p></td></tr>
-        <tr><td id=""><p>a</p></td><td id=""><p>b</p></td><td id=""><p>a</p></td><td id=""><p>b</p></td><td id=""><p>a</p></td><td id=""><p>b</p></td><td id=""><p>a</p></td><td id=""><p>b</p></td><td id=""><p>b</p></td></tr>
+        <tr>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+        </tr>
+        <tr>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+        </tr>
+        <tr>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+            <td class="slot" id=""><p class="cont" id=""></p><span class="popnull" id="">
+                    <p id="">a</p>
+                </span></td>
+        </tr>
         <br>
-        <tr><td id="0"><p>a</p></td><td id="1"><p>b</p></td><td id="2"><p>a</p></td><td id="3"><p>b</p></td><td id="4"><p>a</p></td><td id="5"><p>b</p></td><td id="6"><p>a</p></td><td id="7"><p>b</p></td><td id="8"><p>b</p></td></tr>
+        <tr>
+            <td class="slot" id="0"><p class="cont" id="cont0"></p><span class="popnull" id="popup0">
+                    <p id="nome0">a</p>
+                </span></td>
+            <td class="slot" id="1"><p class="cont" id="cont1"></p><span class="popnull" id="popup1">
+                    <p id="nome1">a</p>
+                </span></td>
+            <td class="slot" id="2"><p class="cont" id="cont2"></p><span class="popnull" id="popup2">
+                    <p id="nome2">a</p>
+                </span></td>
+            <td class="slot" id="3"><p class="cont" id="cont3"></p><span class="popnull" id="popup3">
+                    <p id="nome3">a</p>
+                </span></td>
+            <td class="slot" id="4"><p class="cont" id="cont4"></p><span class="popnull" id="popup4">
+                    <p id="nome4">a</p>
+                </span></td>
+            <td class="slot" id="5"><p class="cont" id="cont5"></p><span class="popnull" id="popup5">
+                    <p id="nome5">a</p>
+                </span></td>
+            <td class="slot" id="6"><p class="cont" id="cont6"></p><span class="popnull" id="popup6">
+                    <p id="nome6">a</p>
+                </span></td>
+            <td class="slot" id="7"><p class="cont" id="cont7"></p><span class="popnull" id="popup7">
+                    <p id="nome7">a</p>
+                </span></td>
+            <td class="slot" id="8"><p class="cont" id="cont8"></p><span class="popnull" id="popup8">
+                    <p id="nome8">a</p>
+                </span></td>
+        </tr>
     </table>
 
     <?php
 
-function getJson(){
-    $pdo = new PDO('mysql:host=localhost;dbname=testeid' , 'root', '');
-    $pdo-> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    function getJson()
+    {
+        $pdo = new PDO('mysql:host=localhost;dbname=testeid', 'root', '');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql_code = "SELECT vetorsql FROM usuarios";
-    $prepare = $pdo->prepare($sql_code);
-    $count = $prepare->execute();
+        $sql_code = "SELECT vetorsql FROM usuarios";
+        $prepare = $pdo->prepare($sql_code);
+        $count = $prepare->execute();
 
-    $json_codificado = $prepare->fetchAll(PDO::FETCH_ASSOC);
+        $json_codificado = $prepare->fetchAll(PDO::FETCH_ASSOC);
 
-    foreach($json_codificado as $json){
-       $jsonCodificado = $json['vetorsql'];
+        foreach ($json_codificado as $json) {
+            $jsonCodificado = $json['vetorsql'];
+        }
+
+        return $jsonCodificado;
     }
 
-    return $jsonCodificado;
-}
+    $jsonCodificado = getJson();
 
-$jsonCodificado = getJson();
+    $array_de_objetos = json_decode($jsonCodificado);
 
-$array_de_objetos = json_decode($jsonCodificado);
-
-if ($array_de_objetos === null) {
-    echo "Erro ao decodificar a string JSON.";
-} else {
-    // Exemplo de como acessar os dados do array de objetos decodificado
-    /* foreach ($array_de_objetos as $objeto) {
-        echo "Slot: " . $objeto->Slot . ", id: " . $objeto->id . ", Count: " . $objeto->Count . ", Damage: " . $objeto->Damage . "<br>";
-        $slots[] = $objeto->Slot; 
-    }*/?>
-    <script>
-        const slots = [];
-        const ids = [];
-        const counts = [];
-        const damages = [];
-    <?php
+    if ($array_de_objetos === null) {
+        echo "Erro ao decodificar a string JSON.";
+    } else {
+        // Exemplo de como acessar os dados do array de objetos decodificado
+        /* foreach ($array_de_objetos as $objeto) {
+            echo "Slot: " . $objeto->Slot . ", id: " . $objeto->id . ", Count: " . $objeto->Count . ", Damage: " . $objeto->Damage . "<br>";
+            $slots[] = $objeto->Slot; 
+        }*/ ?>
+        <script>
+            const slots = [];
+            const ids = [];
+            const counts = [];
+            const damages = [];
+            <?php
 
 
-        foreach($array_de_objetos as $elemento){?>
-
-            slots.push(<?= $elemento->Slot. ", ";?>);
-            ids.push(<?= '"' .$elemento-> id. '" , ';?>);
-            counts.push(<?= $elemento->Count. ", ";?>);
-            damages.push(<?= $elemento->Damage. ", ";?>);
-
-        <?php
-        }
-    ?>
-
-    console.log(slots, ids, counts, damages);
-    for (let i = 0; i < 36; i++) {
-        let newp = document.createElement("img");
-        newp.setAttribute("src", "../../img/iconeitens/item/" + ids[i] + ".png");
-        //newp.innerHTML = ids[i];   more statements
-        item = document.getElementById(slots[i]);
-        if(ids[i] == null){}
-        else{
-        item.appendChild(newp);
-        }
-    }
-    </script>
-<?php } ?>
+            foreach ($array_de_objetos as $elemento) { ?>
+  
+                slots.push(<?= $elemento->Slot . ", "; ?>);
+                    ids.push(<?= '"' . $elemento->id . '" , '; ?>);
+                    counts.push(<?= $elemento->Count . ", "; ?>);
+                    damages.push(<?= $elemento->Damage . ", "; ?>);
+    
+                    <?php
+            }
+            ?>
+    
+            console.log(slots, ids, counts, damages);
+            for (let i = 0; i < 36 ;  i++) {
+                let  newp = document.createElement("img");
+                    newp.className = "item";
+                    newp.setAttribute("src", "../../img/iconeitens/item/" + ids[i] + ".png");
+                    //newp.innerHTML = ids[i];   more statements
+                item = document.getElementById(slots[i]);
+                if(ids[i] == null){}
+            else{
+                item.appendChild(newp);
+                document.getElementById("popup" + i).className = "nomeBloco";
+                document.getElementById("nome" + i).innerHTML = ids[i] + " X" + counts[i];
+                document.getElementById("cont" + i).innerHTML = counts[i];
+                }
+            }
+       
+            </script>
+    <?php } ?>
 </body>
+
 </html>
-
-
-
-
