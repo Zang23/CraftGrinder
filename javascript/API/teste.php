d850418cf7c351656e16baadba31f7faffa04510
Segetto <161674677+Segetto@users.noreply.github.com>
2024-06-09 06:19:08 -0300

    adição dos campos de armadura e escudo no inventário em 'teste.php' e refatoramento de código

--- javascript/API/teste.php
+++ javascript/API/teste.php
@@ -13,123 +13,62 @@
 
 <body>
     <table class="tabela">
-        <tr>
-            
-            <td class="slot" id="9"><p class="cont" id="cont9"></p><span class="popnull" id="popup9">
-                    <p id="nome9">a</p>
-                </span></td>
-            <td class="slot" id="10"><p class="cont" id="cont10"></p><span class="popnull" id="popup10">
-                    <p id="nome10">a</p>
-                </span></td>
-            <td class="slot" id="11"><p class="cont" id="cont11"></p><span class="popnull" id="popup11">
-                    <p id="nome11">a</p>
-                </span></td>
-            <td class="slot" id="12"><p class="cont" id="cont12"></p><span class="popnull" id="popup12">
-                    <p id="nome12">a</p>
-                </span></td>
-            <td class="slot" id="13"><p class="cont" id="cont13"></p><span class="popnull" id="popup13">
-                    <p id="nome13">a</p>
-                </span></td>
-            <td class="slot" id="14"><p class="cont" id="cont14"></p><span class="popnull" id="popup14">
-                    <p id="nome14">a</p>
-                </span></td>
-            <td class="slot" id="15"><p class="cont" id="cont15"></p><span class="popnull" id="popup15">
-                    <p id="nome15">a</p>
-                </span></td>
-            <td class="slot" id="16"><p class="cont" id="cont16"></p><span class="popnull" id="popup16">
-                    <p id="nome16">a</p>
-                </span></td>
-            <td class="slot" id="17"><p class="cont" id="cont17"></p><span class="popnull" id="popup17">
-                    <p id="nome17">a</p>
-                </span></td>
+        <tr class="inventariolinha">
+            <td class="slotarmadura cabeca" id="103"></td>
         </tr>
-        <tr>
-            <td class="slot" id="18"><p class="cont" id="cont18"></p><span class="popnull" id="popup18">
-                    <p id="nome18">a</p>
-                </span></td>
-            <td class="slot" id="19"><p class="cont" id="cont19"></p><span class="popnull" id="popup19">
-                    <p id="nome19">a</p>
-                </span></td>
-            <td class="slot" id="20"><p class="cont" id="cont20"></p><span class="popnull" id="popup20">
-                    <p id="nome20">a</p>
-                </span></td>
-            <td class="slot" id="21"><p class="cont" id="cont21"></p><span class="popnull" id="popup21">
-                    <p id="nome21">a</p>
-                </span></td>
-            <td class="slot" id="22"><p class="cont" id="cont22"></p><span class="popnull" id="popup22">
-                    <p id="nome22">a</p>
-                </span></td>
-            <td class="slot" id="23"><p class="cont" id="cont23"></p><span class="popnull" id="popup23">
-                    <p id="nome23">a</p>
-                </span></td>
-            <td class="slot" id="24"><p class="cont" id="cont24"></p><span class="popnull" id="popup24">
-                    <p id="nome24">a</p>
-                </span></td>
-            <td class="slot" id="25"><p class="cont" id="cont25"></p><span class="popnull" id="popup25">
-                    <p id="nome25">a</p>
-                </span></td>
-            <td class="slot" id="26"><p class="cont" id="cont26"></p><span class="popnull" id="popup26">
-                    <p id="nome26">a</p>
-                </span></td>
+        <tr class="inventariolinha">
+            <td class="slotarmadura peitoral" id="102"></td>
         </tr>
-        <tr>
-            <td class="slot" id="27"><p class="cont" id="cont27"></p><span class="popnull" id="popup27">
-                    <p id="nome27">a</p>
-                </span></td>
-            <td class="slot" id="28"><p class="cont" id="cont28"></p><span class="popnull" id="popup28">
-                    <p id="nome28">a</p>
-                </span></td>
-            <td class="slot" id="29"><p class="cont" id="cont29"></p><span class="popnull" id="popup29">
-                    <p id="nome29">a</p>
-                </span></td>
-            <td class="slot" id="30"><p class="cont" id="cont30"></p><span class="popnull" id="popup30">
-                    <p id="nome30">a</p>
-                </span></td>
-            <td class="slot" id="31"><p class="cont" id="cont31"></p><span class="popnull" id="popup31">
-                    <p id="nome31">a</p>
-                </span></td>
-            <td class="slot" id="32"><p class="cont" id="cont32"></p><span class="popnull" id="popup32">
-                    <p id="nome32">a</p>
-                </span></td>
-            <td class="slot" id="33"><p class="cont" id="cont33"></p><span class="popnull" id="popup33">
-                    <p id="nome33">a</p>
-                </span></td>
-            <td class="slot" id="34"><p class="cont" id="cont34"></p><span class="popnull" id="popup34">
-                    <p id="nome34">a</p>
-                </span></td>
-            <td class="slot" id="35"><p class="cont" id="cont35"></p><span class="popnull" id="popup35">
-                    <p id="nome35">a</p>
-                </span></td>
+        <tr class="inventariolinha">
+            <td class="slotarmadura calca" id="101"></td>
         </tr>
-        <br>
-        <tr>
-            <td class="slot" id="0"><p class="cont" id="cont0"></p><span class="popnull" id="popup0">
-                    <p id="nome0">a</p>
-                </span></td>
-            <td class="slot" id="1"><p class="cont" id="cont1"></p><span class="popnull" id="popup1">
-                    <p id="nome1">a</p>
-                </span></td>
-            <td class="slot" id="2"><p class="cont" id="cont2"></p><span class="popnull" id="popup2">
-                    <p id="nome2">a</p>
-                </span></td>
-            <td class="slot" id="3"><p class="cont" id="cont3"></p><span class="popnull" id="popup3">
-                    <p id="nome3">a</p>
-                </span></td>
-            <td class="slot" id="4"><p class="cont" id="cont4"></p><span class="popnull" id="popup4">
-                    <p id="nome4">a</p>
-                </span></td>
-            <td class="slot" id="5"><p class="cont" id="cont5"></p><span class="popnull" id="popup5">
-                    <p id="nome5">a</p>
-                </span></td>
-            <td class="slot" id="6"><p class="cont" id="cont6"></p><span class="popnull" id="popup6">
-                    <p id="nome6">a</p>
-                </span></td>
-            <td class="slot" id="7"><p class="cont" id="cont7"></p><span class="popnull" id="popup7">
-                    <p id="nome7">a</p>
-                </span></td>
-            <td class="slot" id="8"><p class="cont" id="cont8"></p><span class="popnull" id="popup8">
-                    <p id="nome8">a</p>
-                </span></td>
+        <tr class="inventariolinha fim">
+            <td class="slotarmadura pes" id="100"></td>
+            <td class="slotarmadura escudo" id="-106"></td>
+        </tr>
+        <tr class="inventariolinha"> 
+            <td class="slot" id="9"></td>
+            <td class="slot" id="10"></td>
+            <td class="slot" id="11"></td>
+            <td class="slot" id="12"></td>
+            <td class="slot" id="13"></td>
+            <td class="slot" id="14"></td>
+            <td class="slot" id="15"></td>
+            <td class="slot" id="16"></td>
+            <td class="slot" id="17"></td>
+        </tr>
+        <tr class="inventariolinha">
+            <td class="slot" id="18"></td>
+            <td class="slot" id="19"></td>
+            <td class="slot" id="20"></td>
+            <td class="slot" id="21"></td>
+            <td class="slot" id="22"></td>
+            <td class="slot" id="23"></td>
+            <td class="slot" id="24"></td>
+            <td class="slot" id="25"></td>
+            <td class="slot" id="26"></td>
+        </tr>
+        <tr class="inventariolinha">
+            <td class="slot" id="27"></td>
+            <td class="slot" id="28"></td>
+            <td class="slot" id="29"></td>
+            <td class="slot" id="30"></td>
+            <td class="slot" id="31"></td>
+            <td class="slot" id="32"></td>
+            <td class="slot" id="33"></td>
+            <td class="slot" id="34"></td>
+            <td class="slot" id="35"></td>
+        </tr>
+        <tr class="inventariolinha hotbar">
+            <td class="slot" id="0"></td>
+            <td class="slot" id="1"></td>
+            <td class="slot" id="2"></td>
+            <td class="slot" id="3"></td>
+            <td class="slot" id="4"></td>
+            <td class="slot" id="5"></td>
+            <td class="slot" id="6"></td>
+            <td class="slot" id="7"></td>
+            <td class="slot" id="8"></td>
         </tr>
     </table>
 
@@ -160,16 +99,12 @@
     if ($array_de_objetos === null) {
         echo "Erro ao decodificar a string JSON.";
     } else {
-        // Exemplo de como acessar os dados do array de objetos decodificado
-        /* foreach ($array_de_objetos as $objeto) {
-            echo "Slot: " . $objeto->Slot . ", id: " . $objeto->id . ", Count: " . $objeto->Count . ", Damage: " . $objeto->Damage . "<br>";
-            $slots[] = $objeto->Slot; 
-        }*/ ?>
+       ?>
+
         <script>
             const slots = [];
             const ids = [];
             const counts = [];
-            /* const damages = []; */
             <?php
 
 
@@ -184,19 +119,36 @@
             }
             ?>
     
-            console.log(slots, ids, counts/* , damages */);
-            for (let i = 0; i < 36 ;  i++) {
-                let  newp = document.createElement("img");
-                    newp.className = "item";
-                    newp.setAttribute("src", "../../img/iconeitens/items/" + ids[i] + ".png");
-                    //newp.innerHTML = ids[i];   more statements
-                item = document.getElementById(slots[i]);
-                if(ids[i] == null){}
+            console.log(slots, ids, counts);
+            for (let i = 0; i < 41 ;  i++) {
+
+                const item = document.getElementById(slots[i]);
+
+                if(item == null){}
             else{
-                item.appendChild(newp);
-                document.getElementById("popup" + slots[i]).className = "nomeBloco";
-                document.getElementById("nome" + slots[i]).innerHTML = ids[i] + " X" + counts[i];
-                document.getElementById("cont" + slots[i]).innerHTML = counts[i];
+                let  newimg = document.createElement("img");
+                newimg.className = "item";
+                newimg.setAttribute("src", "../../img/iconeitens/items/" + ids[i] + ".png");
+
+                let newcont = document.createElement("p");
+                newcont.className = "cont";
+                newcont.setAttribute("id", "cont" + i);
+
+                let newnome = document.createElement("p");
+                newnome.setAttribute("id", "nome" + i);
+
+                let newpopup = document.createElement("span");
+                newpopup.setAttribute("id", "popup" + i);
+                newpopup.className = "nomeBloco";
+                item.style.backgroundImage = "none";
+                newnome.innerHTML = ids[i] + " X" + counts[i];
+                newcont.innerHTML = counts[i];
+                item.appendChild(newimg);
+                if(slots[i] <= 36 && slots[i] > 0){
+                item.appendChild(newcont);
+                }
+                item.appendChild(newpopup);
+                newpopup.appendChild(newnome);
                 }
             }
        
