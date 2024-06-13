19312425f141299e9ea1326faf50c853c8008b16
Segetto <161674677+Segetto@users.noreply.github.com>
2024-06-12 21:41:04 -0300

    adição de um icone de bloco de mel na aba do site

--- html/index.php
+++ html/index.php
@@ -19,6 +19,7 @@
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/index.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="icon" href="../img/iconeItens/minecraft/honey_block.png">
     <title>CraftGrinder - Início</title>
 </head>
 <body>
