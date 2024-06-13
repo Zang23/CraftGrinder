98759beb3cd2126205f027ed0e6856cb8f729cb2
Alanis <149546408+AlanisCoutinho@users.noreply.github.com>
2024-06-12 21:42:38 -0300

    layout "editarArtigoScript.php"

--- html/paginaUsuario.php
+++ html/paginaUsuario.php
@@ -40,6 +40,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/paginausuario.css">
+    <link rel="icon" href="../img/iconeItens/minecraft/dirt.png">
     <title>Document</title>     
 </head>
 <body>
