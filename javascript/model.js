f02a870c356f8818f08837cb31b8bb6042c71d8b
Alanis <149546408+AlanisCoutinho@users.noreply.github.com>
2024-06-12 21:02:34 -0300

    modificando o inventario.php

--- javascript/model.js
+++ javascript/model.js
@@ -21,13 +21,8 @@ function updateCube() {
     cubeX += dx * easing;
     cubeY += dy * easing;
 
-<<<<<<< HEAD
-    const corpoY = (cubeX / corpoRect.width - 0.5) * 5; // Ajustando para coordenadas relativas à div
-    const corpoX = -(cubeY / corpoRect.height - 0.5) * 5;
-=======
     const corpoY = (cubeX / corpoRect.width - 0.5) * 2; // Ajustando para coordenadas relativas à div
     const corpoX = -(cubeY / corpoRect.height - 0.5) * 2;
->>>>>>> af6a51e457edc7339d6a8780410e15269a90f4dd
     corpo.style.transform = `rotateX(${corpoX}deg) rotateY(${corpoY}deg)`;
     requestAnimationFrame(updateCube);
 }
